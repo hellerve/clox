90d622c3fdbde5d0b946efecc5168dc184b53216
hellerve <veit@veitheller.de>
2019-01-30 17:36:42 +0100

    main: better usage

--- main.c
+++ main.c
@@ -6,17 +6,23 @@
 #include "src/vm.h"
 
 int main(int argc, const char** argv) {
+  int ret = 0;
   vm* cvm = init_vm();
 
   switch (argc) {
   case 1: repl(cvm); break;
   case 2: run_file(cvm, argv[1]); break;
   default:
-    fprintf(stderr, "Usage: clox [path]\n");
-    exit(64);
+    fprintf(stderr, "Usage: %s [file]\n", argv[0]);
+    fputs(
+      "  The [file] variable is optional.\n"
+      "  Specifying it will result in that file being run, running clox without it will\n"
+      "  start a REPL.\n",
+      stderr);
+    ret = 1;;
   }
 
   free_vm(cvm);
 
-  return 0;
+  return ret;
 }
